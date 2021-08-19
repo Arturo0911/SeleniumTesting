64c71f6bb3d42f24447c7ac1ce1b008dca0f8b9d
Arturo0911 <anegreiross@outlook.com>
2021-08-19 16:02:36 -0500

    ok

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+
+
 import unittest
 from pyunitreport import HTMLTestRunner
 from selenium import webdriver
@@ -12,7 +15,7 @@ class Helloworld(unittest.TestCase):
     def test_hello_world(self):
         driver = self.driver
         driver.get("https://www.platzi.com")
-    
+
     def test_visit_wikipedia(self):
         self.driver.get("https://www.wikipedia.org")
 
@@ -23,3 +26,4 @@ class Helloworld(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main(verbosity=2, testRunner=HTMLTestRunner(output = "reportes", report_name = "hello-world-report"))
+
